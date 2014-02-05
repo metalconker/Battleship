3fc922c4f1b0c1c6fddc69673b919a6a8a319026
robertschneidman <robert.schneidman@gmail.com>
2014-02-05 16:21:05 -0500

    Initial Commit

---
+++ BattleshipApp/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  BattleshipApp
+//
+
+//  Copyright (c) 2014 COMP-361. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ BattleshipApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BattleshipApp
+//
+//  Created by Robert Schneidman on 2/5/2014.
+//  Copyright (c) 2014 COMP-361. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
