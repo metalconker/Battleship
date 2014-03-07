610f98070156fad68c60a4da03881c9204811c75
robertschneidman <robert.schneidman@gmail.com>
2014-03-07 16:29:05 -0500

    asdf

--- Battleship/HelperFunctions/GCHelper.m
+++ Battleship/HelperFunctions/GCHelper.m
@@ -20,5 +20,26 @@ static GCHelper *sharedHelper = nil;
     return sharedHelper;
 }
 
+-(id)init {
+    if ((self = [super init])) {
+        gameCenterAvailable = [self isGameCenerAvailable];
+        if (gameCenterAvailable) {
+            NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+            [nc addObserver:self selector:@selector(authenticationChanged) name:GKPlyaerAuthenticationDidChangeNotificationName object:nil];
+            
+        }
+    }
+    return self;
+}
 
+-(void)authenticationChanged {
+    if ([GKLocalPlayer localPlayer].isAuthenticated && !userAuthenticated) {
+        NSLog(@"Authetication changed: player authenticated");
+        userAuthenticated = TRUE;
+    }
+    else if (![GKLocalPlayer localPlayer].isAuthenticated && userAuthenticated) {
+        NSLog(@"Authentication changed: player not authenticated");
+        userAuthenticated = FALSE;
+    }
+}
 @end
