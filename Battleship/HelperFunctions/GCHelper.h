801976696fd79021a8de2e27a7c8b745aacbf775
micahelbaz <micah.elbaz1@gmail.com>
2014-03-07 17:17:02 -0500

    .h update

--- Battleship/HelperFunctions/GCHelper.h
+++ Battleship/HelperFunctions/GCHelper.h
@@ -8,10 +8,31 @@
 
 #import <Foundation/Foundation.h>
 #import <GameKit/GameKit.h>
-@interface GCHelper : NSObject {
+
+@protocol GCHelperDelegate
+- (void)matchStarted;
+- (void)matchEnded;
+- (void)match:(GKMatch *)match didReceiveData:(NSData *)data
+   fromPlayer:(NSString *)playerID;
+@end
+
+@interface GCHelper : NSObject <GKMatchmakerViewControllerDelegate, GKMatchDelegate> {
     BOOL gameCenterAvailable;
     BOOL userAuthenticated;
+    UIViewController *presentingViewController;
+    GKMatch *match;
+    BOOL matchStarted;
+    id <GCHelperDelegate> delegate;
 }
+
+@property (retain) UIViewController *presentingViewController;
+@property (retain) GKMatch *match;
+@property (assign) id <GCHelperDelegate> delegate;
+
+- (void)findMatchWithMinPlayers:(int)minPlayers maxPlayers:(int)maxPlayers
+                 viewController:(UIViewController *)viewController
+                       delegate:(id<GCHelperDelegate>)theDelegate;
+
 
 @property (assign, readonly) BOOL gameCenterAvailable;
 
